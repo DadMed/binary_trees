f286780b592eff552fc7e521ac287d2b2a72e120
DadMed <mohammed.dad.ens@gmail.com>
2024-03-01 00:01:48 +0100

    all tasks

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -15,18 +15,22 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size)
 	int extract, i = 0;
 	size_t heap_size;
 
-	if (!heap)
+	if (!heap || !size)
 		return (NULL);
+
 	heap_size = binary_tree_size(heap);
 	*size = heap_size;
+
 	array = malloc(heap_size * sizeof(int));
 	if (!array)
 		return (NULL);
+
 	while (heap)
 	{
 		extract = heap_extract(&heap);
 		array[i] = extract;
 		i++;
 	}
+
 	return (array);
 }
